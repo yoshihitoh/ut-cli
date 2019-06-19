d8289a6c506f7ed2c9adf1f3db1e780645a1e932
yoshihitoh <yoshihito.arih@gmail.com>
2019-06-19 22:39:09 +0900

    Add scripts to enable builds on Travis CI.

---
+++ ci/script.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+main() {
+    rustup target add "$TARGET"
+    cargo build --target "$TARGET" --verbose
+    cargo test --target "$TARGET" --all --verbose
+}
+
+main
